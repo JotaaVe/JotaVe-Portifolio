b926ced05f02300c3bc8f0feb0c1b88247d612a4
Jotaave <rd.pedroso.ranciaro@gmail.com>
2023-10-17 14:51:58 -0300

    quack!

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
+/*Escrita automatica*/
 document.addEventListener('DOMContentLoaded', () => {
     new TypeIt(".animated", {
         speed: 200,
         loop: true
-    }).type('JavaScript', {delay: 200}).delete(10).type('Html5', {delay: 500}).delete(5).type('Css')
-    
-    .go()
+    }).type('JavaScript', { delay: 200 }).delete(10).type('Html5', { delay: 500 }).delete(5).type('Css')
+
+        .go()
 })
